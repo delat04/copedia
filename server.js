78c7a0a7ec45cfdbec4e7e1f743d2db2778ce724
delat04 <129554505+delat04@users.noreply.github.com>
2024-05-23 22:06:17 +0100

    Update server.js

--- server.js
+++ server.js
@@ -67,7 +67,7 @@ app.post('/save-marker', (req, res) => {
 });
 
 
-app.post('/get-marker', (req, res) => {
+app.get('/get-marker', (req, res) => {
     const jsonFilePath = path.join(__dirname, 'casas.json');
 
     // Check if the file exists
